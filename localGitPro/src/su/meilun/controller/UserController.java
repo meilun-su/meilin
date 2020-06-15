3acea792d9971f37ccbc2b80c922560e18909c75
meilun-su <MeiLun_Su@foxmail.com>
2020-06-15 20:45:06 +0800

    提交本地仓库

--- localGitPro/src/su/meilun/controller/UserController.java
+++ localGitPro/src/su/meilun/controller/UserController.java
@@ -17,4 +17,46 @@ public class UserController {
 		this.love=love;
 		
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getSex() {
+		return sex;
+	}
+
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+
+	public String getLove() {
+		return love;
+	}
+
+	public void setLove(String love) {
+		this.love = love;
+	}
+	
+	
 }
