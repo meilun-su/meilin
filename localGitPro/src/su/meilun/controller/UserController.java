f9366b9c647df1793b10559fce484adc68ae7376
meilun-su <MeiLun_Su@foxmail.com>
2020-06-15 20:43:05 +0800

    提交到本地仓库

--- localGitPro/src/su/meilun/controller/UserController.java
+++ localGitPro/src/su/meilun/controller/UserController.java
@@ -2,4 +2,19 @@ package su.meilun.controller;
 
 public class UserController {
 
+	private int id;
+	private String username;
+	private String password;
+	private int sex;
+	private String love;
+	
+	public UserController(int id,String username,String password,int sex,String love) {
+		super();
+		this.id=id;
+		this.username=username;
+		this.password=password;
+		this.sex=sex;
+		this.love=love;
+		
+	}
 }
